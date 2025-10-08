fae6682c48a6ad61f8f974031b9a52317d39f5d6
Pacharapon118 <674244118@mail.pbru.ac.th>
2025-10-08 19:42:57 +0700

    Create Child1.c

---
+++ Child1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("My bio.: \n");
+    printf("ID : 674244118 \n");
+    printf("Name : Pacharapon\n");
+    printf("------------------------------\n");
+    getchar();
+    return 0;
+}
